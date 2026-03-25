5fee2b2e9eb37cae8fc1465795bb68253f6e6b39
Hieumaiduc <hieumaiduc2005@gmail.com>
2026-03-25 09:05:50 +0700

    fix lỗi trống header

--- QuanLyLopHoc/core/Controller.php
+++ QuanLyLopHoc/core/Controller.php
@@ -1,10 +1,13 @@
 <?php
 class Controller {
     protected function view($path, $data = []) {
+        ob_start(); 
         extract($data);
         require __DIR__ . '/../views/' . $path . '.php';
+        ob_end_flush();
     }
     protected function redirect($url) {
+        if (ob_get_length()) ob_clean();       
         header("Location: index.php?controller=" . $url);
         exit();
     }
--- QuanLyLopHoc/services/MonHocService.php
+++ QuanLyLopHoc/services/MonHocService.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once __DIR__ . '/../dao/MonHocDAO.php';
 class MonHocService {
